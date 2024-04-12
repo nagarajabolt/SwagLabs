7fe4e4329007aca2e2ceeba5beba78a2134a4a63
naga <naga@gmail.com>
2024-04-12 10:57:02 +0530

    Extent Reports

--- SwagLabs/src/test/java/testcases/Scenarios.java
+++ SwagLabs/src/test/java/testcases/Scenarios.java
@@ -14,8 +14,10 @@ import objects.HomePage;
 import objects.ProductPojo;
 import objects.ProductsPage;
 import testData.TestData;
+
 /**
- * This class contains all positive scenarios (plus 1 deliberate -ve scenario for demo) like login flow, sorting, adding to cart, checkout
+ * This class contains all positive scenarios (plus 1 deliberate -ve scenario
+ * for demo) like login flow, sorting, adding to cart, checkout
  * 
  */
 public class Scenarios extends BaseClass {
@@ -24,10 +26,8 @@ public class Scenarios extends BaseClass {
 	CheckoutPage cp = new CheckoutPage();
 	TestData testData = new TestData();
 
-	
-	@BeforeMethod(description = "Logs into the application", groups = {"sanity", "regression", "positive", "e2e"})
-	public void loginToApplication()
-	{		
+	@BeforeMethod(description = "Logs into the application", groups = { "sanity", "regression", "positive", "e2e" })
+	public void loginToApplication() {
 		Log.info("<---------> Login to the application <---------> ");
 		webDriverOperations.instantiateBrowser(browser, headless);
 		webDriverOperations.navigateToUrl(url);
@@ -36,54 +36,53 @@ public class Scenarios extends BaseClass {
 		webDriverOperations.click(hp.homePage_login_button());
 		webDriverOperations.waitForElementTobeDisplayed(pp.swag_Labs_Logo());
 
-		Log.info("<---------> Login to the application <---------> ");		
-		webDriverOperations.sleep(5);
+		Log.info("<---------> Login to the application <---------> ");
 	}
-	
-	@AfterMethod(description = "Quits browser", groups = {"sanity", "regression", "positive", "e2e"})
+
+	@AfterMethod(description = "Quits browser", groups = { "sanity", "regression", "positive", "e2e" })
 	public void cleanUp(ITestResult result) {
-		webDriverOperations.quitBrowser();	
-	if(result.getStatus()==ITestResult.FAILURE) {
-		test.log(LogStatus.FAIL, "Testcase failed");
+		webDriverOperations.quitBrowser();
+		if (result.getStatus() == ITestResult.FAILURE) {
+			test.log(LogStatus.FAIL, "Testcase failed");
+		} else {
+			test.log(LogStatus.PASS, "Testcase passed");
+		}
+		extent.endTest(test);
+		test = null;
 	}
-	else {
-		test.log(LogStatus.PASS, "Testcase passed");
-	}
-	extent.endTest(test);
-	test = null;
-	}
-	
-	
-	@Test (priority = 0, enabled = true, description = "Checks if logo is displayed after login", groups = {"sanity", "regression", "positive", "e2e"})
-	public void loginFlow()
-	{				
+
+	@Test(priority = 0, enabled = true, description = "Checks if logo is displayed after login", groups = { "sanity",
+			"regression", "positive", "e2e" })
+	public void loginFlow() {
 		Log.info("<---------> Testcase - loginFlow <---------> ");
 		test = extent.startTest("loginFlow");
-		Assert.assertTrue(webDriverOperations.verifyElementIsDisplayed(pp.swag_Labs_Logo()), "Swag Labs logo is not displayed");
-		Assert.assertTrue((webDriverOperations.verifyElementTextIsMatching(pp.swag_Labs_Logo(), testData.home_logoText)), "Swag Labs text is not matching");
+		Assert.assertTrue(webDriverOperations.verifyElementIsDisplayed(pp.swag_Labs_Logo()),
+				"Swag Labs logo is not displayed");
+		Assert.assertTrue(
+				(webDriverOperations.verifyElementTextIsMatching(pp.swag_Labs_Logo(), testData.home_logoText)),
+				"Swag Labs text is not matching");
 		Log.info("<---------> Testcase - loginFlow <---------> ");
-
+		test.log(LogStatus.PASS, "loginFlow testcase is passed");		
 
 	}
-	
-	
-	@Test (priority = 1, enabled = true, dependsOnMethods = "loginFlow", description = "Checks if products are listed correctly", groups = {"sanity", "regression", "positive"})
-	public void productListingVerification()
-	{
+
+	@Test(priority = 1, enabled = true, dependsOnMethods = "loginFlow", description = "Checks if products are listed correctly", groups = {
+			"sanity", "regression", "positive" })
+	public void productListingVerification() {
 		Log.info("<---------> Testcase - productListingVerification <---------> ");
 		test = extent.startTest("productListingVerification");
 		ArrayList<ProductPojo> productsActual = webDriverOperations.returnAllListedProducts();
-		Boolean productsMatching = productUtilities.verifyProductListsAreMatching(productsActual, productsExpected);		
+		Boolean productsMatching = productUtilities.verifyProductListsAreMatching(productsActual, productsExpected);
 		Assert.assertTrue(productsMatching, "Products are not matching with expected list");
 		Log.info("Products are displayed correctly");
 		Log.info("<---------> Testcase - productListingVerification <---------> ");
 		test.log(LogStatus.PASS, "productListingVerification testcase is passed");
 
 	}
-	
-	@Test (priority = 2, enabled = true, dependsOnMethods = "loginFlow", description = "Checks if products are sorted correctly on different options of product and price", groups = {"sanity, regression"})
-	public void sortingCheck()
-	{
+
+	@Test(priority = 2, enabled = true, dependsOnMethods = "loginFlow", description = "Checks if products are sorted correctly on different options of product and price", groups = {
+			"sanity, regression" })
+	public void sortingCheck() {
 		Log.info("<---------> Testcase - sortingCheck <---------> ");
 		test = extent.startTest("sortingCheck");
 		ArrayList<ProductPojo> productsExpectedsorted = productsExpected;
@@ -92,113 +91,118 @@ public class Scenarios extends BaseClass {
 		productUtilities.displayListItems(productsExpectedsorted);
 		webDriverOperations.selectDropDownItem(pp.sortingList(), testData.product_sort_name_aToz);
 		ArrayList<ProductPojo> productsActual = webDriverOperations.returnAllListedProducts();
-		Boolean productsMatching = productUtilities.verifyProductListsAreMatching(productsActual, productsExpectedsorted);		
+		Boolean productsMatching = productUtilities.verifyProductListsAreMatching(productsActual,
+				productsExpectedsorted);
 		Assert.assertEquals(productsMatching, true);
 		Log.info("Products are sorted correctly");
-		
+
 		productUtilities.reverseSortByProductName(productsExpectedsorted);
-		Log.info("Arraylist is sorted by descending order of Product Name");		
+		Log.info("Arraylist is sorted by descending order of Product Name");
 		productUtilities.displayListItems(productsExpectedsorted);
-		
-		webDriverOperations.selectDropDownItem(pp.sortingList(), testData.product_sort_name_zToa);		
+
+		webDriverOperations.selectDropDownItem(pp.sortingList(), testData.product_sort_name_zToa);
 		productsActual = webDriverOperations.returnAllListedProducts();
-		productsMatching = productUtilities.verifyProductListsAreMatching(productsActual, productsExpectedsorted);		
+		productsMatching = productUtilities.verifyProductListsAreMatching(productsActual, productsExpectedsorted);
 		Assert.assertEquals(productsMatching, true);
-		Log.info("Products are sorted correctly");		
-		
+		Log.info("Products are sorted correctly");
+
 		productUtilities.sortByPrice(productsExpectedsorted);
 		Log.info("Arraylist is sorted by ascending order of Price");
 		productUtilities.displayListItems(productsExpectedsorted);
-		
-		webDriverOperations.selectDropDownItem(pp.sortingList(), testData.product_sort_price_lowToHigh);		
+
+		webDriverOperations.selectDropDownItem(pp.sortingList(), testData.product_sort_price_lowToHigh);
 		productsActual = webDriverOperations.returnAllListedProducts();
-		productsMatching = productUtilities.verifyProductPricesAreMatching(productsActual, productsExpectedsorted);		
+		productsMatching = productUtilities.verifyProductPricesAreMatching(productsActual, productsExpectedsorted);
 		Assert.assertEquals(productsMatching, true);
-		Log.info("Products are sorted correctly");	
-		
-		
+		Log.info("Products are sorted correctly");
+
 		productUtilities.reversesortByPrice(productsExpectedsorted);
 		Log.info("Arraylist is sorted by descending order of Price");
 		productUtilities.displayListItems(productsExpectedsorted);
-		
-		webDriverOperations.selectDropDownItem(pp.sortingList(), testData.product_sort_price_highToLow);		
+
+		webDriverOperations.selectDropDownItem(pp.sortingList(), testData.product_sort_price_highToLow);
 		productsActual = webDriverOperations.returnAllListedProducts();
-		productsMatching = productUtilities.verifyProductPricesAreMatching(productsActual, productsExpectedsorted);		
+		productsMatching = productUtilities.verifyProductPricesAreMatching(productsActual, productsExpectedsorted);
 		Assert.assertEquals(productsMatching, true);
-		Log.info("Products are sorted correctly");	
-		webDriverOperations.sleep(5);
+		Log.info("Products are sorted correctly");
 		Log.info("<---------> Testcase - sortingCheck <---------> ");
+		test.log(LogStatus.PASS, "sortingCheck testcase is passed");
 	}
-	
-	@Test (priority = 3, enabled = true, dependsOnMethods = "loginFlow", description = "Checks if products are added to cart", groups = {"sanity", "regression"})
-	public void addingToCart()
-	{
+
+	@Test(priority = 3, enabled = true, dependsOnMethods = "loginFlow", description = "Checks if products are added to cart", groups = {
+			"sanity", "regression" })
+	public void addingToCart() {
 		Log.info("<---------> Testcase - addingToCart <---------> ");
 		test = extent.startTest("addingToCart");
 		webDriverOperations.addProductToCart(0);
 		webDriverOperations.click(pp.cartButton());
-		Assert.assertEquals(webDriverOperations.returnNumberOfItemsInCart()==1, true);
+		Assert.assertEquals(webDriverOperations.returnNumberOfItemsInCart() == 1, true);
 		Log.info("Number of items in cart is matching");
 		webDriverOperations.click(pp.removeButton());
 		Log.info("<---------> Testcase - addingToCart <---------> ");
-		extent.endTest(test);
-		extent.flush();
+		test.log(LogStatus.PASS, "addingToCart testcase is passed");		
 	}
 
-	@Test (priority = 4, enabled = true, dependsOnMethods = "loginFlow", description = "Checks if user is able to checkout products", groups = {"sanity", "regression"})
-	public void checkOut()
-	{
-		Log.info("<---------> Testcase - checkOut <---------> ");	
+	@Test(priority = 4, enabled = true, dependsOnMethods = "loginFlow", description = "Checks if user is able to checkout products", groups = {
+			"sanity", "regression" })
+	public void checkOut() {
+		Log.info("<---------> Testcase - checkOut <---------> ");
 		test = extent.startTest("checkOut");
 		webDriverOperations.addProductToCart(0);
-		webDriverOperations.click(pp.cartButton());		
-		Assert.assertTrue(webDriverOperations.returnNumberOfItemsInCart()==1, "Number of products in cart is not matching with expected value");		
+		webDriverOperations.click(pp.cartButton());
+		Assert.assertTrue(webDriverOperations.returnNumberOfItemsInCart() == 1,
+				"Number of products in cart is not matching with expected value");
 		webDriverOperations.click(cp.checkOutButton());
 		webDriverOperations.enterEdit(cp.firstName(), testData.checkout_FirstName);
-		webDriverOperations.enterEdit(cp.lastName(), testData.checkout_LastName);	
-		webDriverOperations.enterEdit(cp.postalCode(), testData.checkout_PostalCode);		
+		webDriverOperations.enterEdit(cp.lastName(), testData.checkout_LastName);
+		webDriverOperations.enterEdit(cp.postalCode(), testData.checkout_PostalCode);
 		webDriverOperations.click(cp.continueButton());
 		webDriverOperations.click(cp.finishButton());
-		Assert.assertTrue(webDriverOperations.verifyElementIsDisplayed(pp.thankYouForOrderMessage()), "Check out complete message is not dispalyed");		
-		webDriverOperations.click(cp.back_to_home());		
+		Assert.assertTrue(webDriverOperations.verifyElementIsDisplayed(pp.thankYouForOrderMessage()),
+				"Check out complete message is not dispalyed");
+		webDriverOperations.click(cp.back_to_home());
 		Log.info("User is able to checkout product");
-		Log.info("<---------> Testcase - checkOut <---------> ");	
-		extent.endTest(test);
-		extent.flush();
-	}	
-	
-	@Test (priority = 5, enabled = true, dependsOnMethods = "loginFlow", description = "Checks if user is able to checkout first 3 least priced products", groups = {"sanity", "regression", "e2e"})
-	public void additional_Case()
-	{
-		Log.info("<---------> Testcase - additional_Case <---------> ");	
+		Log.info("<---------> Testcase - checkOut <---------> ");
+		test.log(LogStatus.PASS, "checkOut testcase is passed");			
+	}
+
+	@Test(priority = 5, enabled = true, dependsOnMethods = "loginFlow", description = "Checks if user is able to checkout first 3 least priced products", groups = {
+			"sanity", "regression", "e2e" })
+	public void additional_Case() {
+		Log.info("<---------> Testcase - additional_Case <---------> ");
 		test = extent.startTest("additional_Case");
 		webDriverOperations.selectDropDownItem(pp.sortingList(), testData.product_sort_price_lowToHigh);
 		webDriverOperations.addProductToCart(0);
 		webDriverOperations.addProductToCart(1);
 		webDriverOperations.addProductToCart(2);
-		webDriverOperations.click(pp.cartButton());	
-		Assert.assertTrue(webDriverOperations.returnNumberOfItemsInCart()==3, "Number of products in cart is not matching with expected value");			
+		webDriverOperations.click(pp.cartButton());
+		Assert.assertTrue(webDriverOperations.returnNumberOfItemsInCart() == 3,
+				"Number of products in cart is not matching with expected value");
 		webDriverOperations.click(cp.checkOutButton());
 		webDriverOperations.enterEdit(cp.firstName(), testData.checkout_FirstName);
-		webDriverOperations.enterEdit(cp.lastName(), testData.checkout_LastName);	
-		webDriverOperations.enterEdit(cp.postalCode(), testData.checkout_PostalCode);		
+		webDriverOperations.enterEdit(cp.lastName(), testData.checkout_LastName);
+		webDriverOperations.enterEdit(cp.postalCode(), testData.checkout_PostalCode);
 		webDriverOperations.click(cp.continueButton());
-		webDriverOperations.click(cp.finishButton());	
-		Assert.assertTrue(webDriverOperations.verifyElementIsDisplayed(pp.thankYouForOrderMessage()), "Check out complete message is not dispalyed");
+		webDriverOperations.click(cp.finishButton());
+		Assert.assertTrue(webDriverOperations.verifyElementIsDisplayed(pp.thankYouForOrderMessage()),
+				"Check out complete message is not dispalyed");
 		Log.info("User is able to checkout 3 products");
-		Log.info("<---------> Testcase - additional_Case <---------> ");	
+		Log.info("<---------> Testcase - additional_Case <---------> ");
+		test.log(LogStatus.PASS, "additional_Case testcase is passed");			
 	}
-	
-	@Test (priority = 6, enabled = true, description = "Purposely fails login flow for demo", groups = {"sanity", "regression", "positive", "e2e"})
-	public void purposelyFailLoginFlow()
-	{				
+
+	@Test(priority = 6, enabled = true, description = "Purposely fails login flow for demo", groups = { "sanity",
+			"regression", "positive", "e2e" })
+	public void purposelyFailLoginFlow() {
 		Log.info("<---------> Testcase - purposelyFailloginFlow <---------> ");
-		test = extent.startTest("loginFlow");
-		Assert.assertTrue(webDriverOperations.verifyElementIsDisplayed(pp.swag_Labs_Logo()), "Swag Labs logo is not displayed");
-		Assert.assertTrue((webDriverOperations.verifyElementTextIsMatching(pp.swag_Labs_Logo(), testData.home_logoText_Fail)), "Swag Labs text is not matching");
+		test = extent.startTest("purposelyFailLoginFlow");
+		Assert.assertTrue(webDriverOperations.verifyElementIsDisplayed(pp.swag_Labs_Logo()),
+				"Swag Labs logo is not displayed");
+		Assert.assertTrue(
+				(webDriverOperations.verifyElementTextIsMatching(pp.swag_Labs_Logo(), testData.home_logoText_Fail)),
+				"Swag Labs text is not matching");
 		Log.info("<---------> Testcase - purposelyFailLoginFlow <---------> ");
-
-
+		test.log(LogStatus.PASS, "purposelyFailLoginFlow testcase is passed");	
 	}
-	
+
 }
